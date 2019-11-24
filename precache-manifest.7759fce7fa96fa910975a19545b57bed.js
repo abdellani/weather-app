8bf019bfd3c3abf9f06a5d52e2378f4d6be2a280
abdellani <abdellani@gmail.com>
2019-11-24 15:27:20 +0100

    Updates

---
+++ precache-manifest.7759fce7fa96fa910975a19545b57bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b05b0b1d94cf678ce03a00249ae846",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e4aa0ce04290d044dbc2",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "361900f6c3a9694b1225",
+    "url": "/weather-app/static/css/main.1a35354f.chunk.css"
+  },
+  {
+    "revision": "e4aa0ce04290d044dbc2",
+    "url": "/weather-app/static/js/2.56365ccb.chunk.js"
+  },
+  {
+    "revision": "361900f6c3a9694b1225",
+    "url": "/weather-app/static/js/main.cf702477.chunk.js"
+  },
+  {
+    "revision": "8d4f78329a9184025652",
+    "url": "/weather-app/static/js/runtime-main.099c227e.js"
+  }
+]);
